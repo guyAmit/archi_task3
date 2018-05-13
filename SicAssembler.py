5e79b4a11635a937ed39f78aa4e7e6b1eba37028
omri <omrieitan@gmail.com>
2018-05-13 17:59:08 +0300

    python translate

--- SicAssembler.py
+++ SicAssembler.py
@@ -10,30 +10,37 @@ file = open("sic.txt", "r")
 
 # read all labels:
 for l in file.readlines():
-    lableMatcher = labelP.fullmatch(l)
-    if lableMatcher.groups() is not None:
+    lableMatcher = labelP.fullmatch(str(l))
+    if lableMatcher is not None:
+        print(l)
         offset += 1
         lablelAndNumber = l.split(' ')
         addressesMap[lablelAndNumber[0][:-1]] = lablelAndNumber[1]
     else:
         offset += 3
 
+print(addressesMap)
 # translate into a new file
 # Read in the file
-final = ""
-lines = file.readlines()
-for i in lines:
-    thisline = i.split(" ")
-    for j in thisline:
-        try:
-            int(s)
-            final += s + " "
-        except ValueError:
-            final += addressesMap[j] + " "
+file.close()
+file = open("sic.txt", "r")
 
-outfile = open("sicTranslation.sic", "w+")
 
-outfile.write(final)
+# final = ""
+# lines = file.readlines()
+# for i in lines:
+#     thisline = i.split(" ")
+#     for j in thisline:
+#         try:
+#             int(j)
+#             final += j + " "
+#         except ValueError:
+#             if j != "sbn":
+#                 final += addressesMap[j] + " "
+#
+# outfile = open("sicTranslation.sic", "w+")
+#
+# outfile.write(final)
 
-outfile.close()
+# outfile.close()
 file.close()
